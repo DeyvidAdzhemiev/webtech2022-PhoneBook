ff6da54e4b9c02bdbbfc61e7bf22f9982a8d8d55
DeyvidAdzhemiev <60064484+DeyvidAdzhemiev@users.noreply.github.com>
2022-04-22 20:16:57 +0300

    Added event on Add button

--- phone book - all/public/functions/index.js
+++ phone book - all/public/functions/index.js
@@ -59,3 +59,34 @@ function homeBtn() {
 
 
 }
+
+
+function submitUser(){
+
+    const items = Array.from(
+        document.querySelector('ul').childNodes).map(li => li.textContent);
+
+
+        const firstnameUser = document.getElementById("firstname").value;
+        const lastnameUser = document.getElementById("firstname").value;
+        const addressUser = document.getElementById("firstname").value;
+        const emailUser = document.getElementById("firstname").value;
+        const phoneUser = document.getElementById("firstname").value;
+        const firstnameUser = document.getElementById("firstname").value;
+
+
+    fetch('http://localhost:3000/newContact', {
+        method: 'POST',
+        body: JSON.stringify({ firstname: firstname,
+                                lastname: lastname,
+                                address: address,
+                                email: email,
+                                phone: phone }),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+
+}
